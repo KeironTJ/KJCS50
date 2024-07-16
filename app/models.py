c5710b1460a697b7e7b0d7f7f3fa9beb23d890dc
Keiron Jones <135234970+KeironTJ@users.noreply.github.com>
2024-07-16 13:07:33 +0000

    Continued setup for Flask, user authentication, registration added.

--- app/models.py
+++ app/models.py
@@ -1,15 +1,28 @@
 from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app import db
+from sqlalchemy import Column, Integer, String, DateTime # type: ignore
+import sqlalchemy.orm as so # type: ignore
+from flask_login import UserMixin # type: ignore
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
 
-class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(Integer, primary_key=True)
+    username = db.Column(String(64), index=True, unique=True)
+    email = db.Column(String(120), index=True, unique=True)
+    password_hash = db.Column(String(256))
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- runserver.py
+++ runserver.py
@@ -1,1 +1,9 @@
-from app import app
+import sqlalchemy as sa #type: ignore
+import sqlalchemy.orm as so #type: ignore
+from app import app, db
+
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db}
